65029f678433268f27207ca95df9e957db94767c
gedesigntech <henry.navarro@ge.com>
2016-02-10 13:45:34 -0800

    custom tests for px-context-browser

--- test/px-context-browser-base-tests.js
+++ test/px-context-browser-base-tests.js
@@ -2,12 +2,307 @@
 // name provided to the pxtestkit yeoman generator
 var px_context_browser_1;
 
+var browserContext = {
+"data": [
+    {
+        "identifier": "001-1",
+        "name": "Lots of Children",
+        "isOpenable" : true,
+        "hasChildren": true
+    },
+    {
+      "identifier": "001-2",
+      "name": "Deep nested",
+      "isOpenable" : true,
+      "children": [
+            {
+                "identifier": "001-2a",
+                "name": "Nested Child 1",
+                "isOpenable" : true,
+                "children": [
+                  {
+                    "identifier": "001-2aa",
+                    "name": "Nested Grandchild 1",
+                    "isOpenable" : true,
+                    "children" : [{
+                      "identifier": "001-2aba",
+                      "name": "Nested Great Grandchild 1",
+                      "isOpenable" : true
+                    },
+                    {
+                      "identifier": "001-2abb",
+                      "name": "Nested Great Grandchild 2",
+                      "isOpenable" : true
+                    },
+                    {
+                      "identifier": "001-2abc",
+                      "name": "Nested Great Grandchild 3",
+                      "isOpenable" : true,
+                      "selectedAsset": true
+                    }],
+                    "meta": { "parentId": "001-2ab" }
+                  },
+                  {
+                    "identifier": "001-2ab",
+                    "name": "Nested Grandchild 2",
+                    "isOpenable" : true
+                  },
+                  {
+                    "identifier": "001-2ac",
+                    "name": "Nested Grandchild 3",
+                    "isOpenable" : true
+                  }
+                ],
+                "meta": { "parentId": "001-2a" }
+            },
+            {
+              "identifier": "001-2b",
+              "name": "Nested Child 2",
+              "isOpenable" : true
+            },
+            {
+              "identifier": "001-2c",
+              "name": "Nested Child 3",
+              "isOpenable" : true
+            }
+        ],
+        "meta": { "parentId": "001-2" }
+    },
+    {
+    "identifier": "001-3",
+    "name": "Nothing Below Me",
+    "isOpenable" : true
+    },
+    {
+    "identifier": "001-4",
+    "name": "Nothing Below, Not openable"
+   }
+],
+"meta": { "parentId": null }
+};
+
+var lotsOfChildren = {
+  "data": [
+    {
+      "name": "A",
+      "identifier": "001-1a"
+    }, {
+      "name": "B",
+      "identifier": "001-1b"
+    }, {
+      "name": "C",
+      "identifier": "001-1c"
+    }, {
+      "name": "D",
+      "identifier": "001-1d"
+    }, {
+      "name": "E",
+      "identifier": "001-1e"
+    }, {
+      "name": "F",
+      "identifier": "001-1f"
+    }, {
+      "name": "G",
+      "identifier": "001-1g"
+    }, {
+      "name": "H",
+      "identifier": "001-1h"
+    }, {
+      "name": "I",
+      "identifier": "001-1i"
+    }, {
+      "name": "AA",
+      "identifier": "001-1aa"
+    }, {
+      "name": "BB",
+      "identifier": "001-1bb"
+    }, {
+      "name": "CC",
+      "identifier": "001-1cc"
+    }, {
+      "name": "DD",
+      "identifier": "001-1dd"
+    }, {
+      "name": "EE",
+      "identifier": "001-1ee"
+    }, {
+      "name": "FF",
+      "identifier": "001-1ff"
+    }, {
+      "name": "GG",
+      "identifier": "001-1gg"
+    }, {
+      "name": "HH",
+      "identifier": "001-1hh"
+    }, {
+      "name": "II",
+      "identifier": "001-1ii"
+    }, {
+      "name": "AAA",
+      "identifier": "001-1aaa"
+    }, {
+      "name": "BBB",
+      "identifier": "001-1bbb"
+    }, {
+      "name": "CCC",
+      "identifier": "001-1ccc"
+    }, {
+      "name": "DDD",
+      "identifier": "001-1ddd"
+    }, {
+      "name": "EEE",
+      "identifier": "001-1eee"
+    }, {
+      "name": "FFF",
+      "identifier": "001-1fff"
+    }, {
+      "name": "GGG",
+      "identifier": "001-1ggg"
+    }, {
+      "name": "HHH",
+      "identifier": "001-1hhh"
+    }, {
+      "name": "III",
+      "identifier": "001-1iii"
+    }
+  ],
+  "meta": {
+    "parentId": "001-1"
+  }
+};
+
+var deepNestedChildren = {
+  "data": [{
+    "name": "Nested Child 1",
+    "identifier": "001-2a",
+    "isOpenable": true,
+    "hasChildren": true
+  },
+  {
+    "name": "Nested Child 2",
+    "identifier": "001-2b",
+    "isOpenable": true
+  },
+  {
+    "name": "Nested Child 3",
+    "identifier": "001-2c",
+    "isOpenable": true
+  }],
+  "meta": {
+    "parentId": "001-2"
+  }
+};
+
+var deepNestedGrandchildren = {
+  "data": [
+    {
+      "name": "Nested Grandchild 1",
+      "identifier": "001-2aa",
+      "isOpenable": true,
+      "hasChildren": true
+    },{
+      "name": "Nested Grandchild 2",
+      "identifier": "001-2ab",
+      "isOpenable": true
+    },
+    {
+      "name": "Nested Grandchild 3",
+      "identifier": "001-2ac",
+      "isOpenable": true
+    }
+  ],
+  "meta": {
+    "parentId": "001-2a"
+  }
+};
+
+var deepNestedGreatGrandchild = {
+  "data": [{
+    "name": "Nested Great Grandchild 1",
+    "identifier": "001-2aba",
+    "isOpenable": true
+  },
+  {
+    "name": "Nested Great Grandchild 2",
+    "identifier": "001-2abb",
+    "isOpenable": true
+  },
+  {
+    "name": "Nested Great Grandchild 3",
+    "identifier": "001-2abc",
+    "isOpenable": true
+  }],
+  "meta": {
+    "parentId": "001-2aa"
+  }
+};
+
+var mockChildren = {
+  "001-1": lotsOfChildren,
+  "001-2": deepNestedChildren,
+  "001-2a": deepNestedGrandchildren,
+  "001-2aa": deepNestedGreatGrandchild
+};
+
+function demoGetChildren(node) {
+  var nodeId = node.identifier,
+      deferred = Q.defer(),
+      nodeIds = {
+        "001-1": "../lotsOfChildren.json",
+        "001-2": "../deepNestedChildren.json",
+        "001-2a":"../deepNestedGrandchildren.json",
+        "001-2aa": "../deepNestedGreatGrandchild.json"
+      };
+  if (nodeIds[nodeId]) {
+    setTimeout(function() {
+      deferred.resolve(mockChildren[nodeId])
+    }, 50);
+  }
+  else {
+    deferred.resolve({ data: [], meta: { parentId: nodeId } });
+  }
+  //don't forget to return the promise!
+  return deferred.promise;
+}
+
 // This is the bootstrapping function that will run the base and custom tests
 // upon the completion of web components construction by Polymer
 document.addEventListener("WebComponentsReady", function() {
+  var contextBrowser = document.querySelector('#px_context_browser_1');
+  contextBrowser.handlers = {
+    getChildren: function(parent, newIndex) {
+      return demoGetChildren(parent);
+    },
+    itemOpenHandler: function(context) {
+      console.log('Opened: ', context);
+    }
+  };
   runBaseTests();
   runCustomTests();
 });
+
+// This function replaces the first character of a CSS selector segment that
+// starts with a numeric digit with the Unicode representation, so that it
+// can be accepted as valid by functions such as 'querySelector()'
+function unicodeNumericSelectorStart(cssSelectorSegment) {
+  var validCSSSelector = "", digitsArray = "0123456789", selectorStart = "";
+  // if input is bad, just give it back
+  if (cssSelectorSegment === null || cssSelectorSegment === "" || !(typeof cssSelectorSegment === "string") ) {
+    return cssSelectorSegment;
+  }
+  // get first character of input
+  selectorStart = cssSelectorSegment.substring(0,1);
+  // if first character is digit
+  if (digitsArray.indexOf(selectorStart) >= 0) {
+    // replace it with corresponding Unicode string
+    validCSSSelector = "\\\\3" + selectorStart + " " + cssSelectorSegment.substring(1);
+  }
+  // else just return the same input
+  else {
+    validCSSSelector = cssSelectorSegment;
+  }
+  return validCSSSelector;
+}
 
 // This is a utility/wrapper function for the test() function of
 // web-component-tester;  the developer can use this to specify tests
@@ -180,6 +475,8 @@ function testCase(options) {
 // generated by the pxtestkit yeoman generator
 function runBaseTests() {
   px_context_browser_1 = document.getElementById('px_context_browser_1');
+
+  px_context_browser_1.browserContext = browserContext;
 
   suite('Base Automation Tests for px-context-browser', function() {
 
