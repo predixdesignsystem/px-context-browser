2d5f769cc715a3de4c23f494f5d0f7d30804ee00
Runn Vermel <runn.vermel@ge.com>
2016-12-27 13:44:29 -0800

    trying a fix on tests

--- test/px-context-typeahead-custom-tests.js
+++ test/px-context-typeahead-custom-tests.js
@@ -1,17 +1,17 @@
 // This is the wrapper for custom tests, called upon web components ready state
 function runCustomTests() {
-  var selectContext = Polymer.dom(document).querySelector('#px_context_browser_1');
+
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
 
    suite('Custom Automation Tests for px-context-browser with typeahead functionality', function() {
+     var selectContext = Polymer.dom(document).querySelector('#px_context_browser_1');
      test('Check that clicking on the header shows the search box', function(done) {
-      var clickableHeader = Polymer.dom(selectContext.root).querySelector('h1');
+      var clickableHeader = selectContext.querySelector('h1');
       clickableHeader.addEventListener('click', function expandListener() {
         flush(function() {
-          var columnsContainer = Polymer.dom(selectContext.root).querySelector('#columnBrowser');
-          var pxContextTypeaheadComp = Polymer.dom(columnsContainer).querySelector('.px-context-browser-typeahead-0');
-          var searchTextbox = Polymer.dom(pxContextTypeaheadComp.root).querySelector('.px-context-browser-typeahead-searchbox');
+          var columnsContainer = selectContext.querySelector('#columnBrowser');
+          var searchTextbox = columnsContainer.querySelector('.px-context-browser-typeahead-searchbox');
           var textVal = searchTextbox.getAttribute('placeholder');
           assert.isTrue(textVal === "Search..");
           done();
@@ -21,12 +21,11 @@ function runCustomTests() {
     });
 
      test('Check that  typing on the search box shows the close icon', function(done) {
-       var columnsContainer = Polymer.dom(selectContext.root).querySelector('#columnBrowser');
-       var pxContextTypeaheadComp = Polymer.dom(columnsContainer).querySelector('.px-context-browser-typeahead-0');
-       var searchTextbox = Polymer.dom(pxContextTypeaheadComp.root).querySelector('.px-context-browser-typeahead-searchbox');
+       var selectContext = Polymer.dom(document).querySelector('#px_context_browser_1');
+       var searchTextbox = Polymer.dom(selectContext).querySelector('.px-context-browser-typeahead-searchbox');
        searchTextbox.addEventListener('input', function(e){
          flush(function() {
-           var closeIcon = Polymer.dom(pxContextTypeaheadComp.root).querySelector('.px-context-browser-typeahead-close-icon');
+           var closeIcon = selectContext.querySelector('.px-context-browser-typeahead-close-icon');
            assert.isTrue(closeIcon.hidden === false);
            done();
          });
@@ -36,13 +35,12 @@ function runCustomTests() {
      });
 
      test('Check that  typing on the search box filters the list', function(done) {
-       var columnsContainer = Polymer.dom(selectContext.root).querySelector('#columnBrowser');
-       var pxContextTypeaheadComp = Polymer.dom(columnsContainer).querySelector('div.columns-3.column-index-2>div:nth-child(3).px-context-browser px-context-browser-typeahead');
-       var searchTextboxfilter = Polymer.dom(pxContextTypeaheadComp.root).querySelector('input.px-context-browser-typeahead-searchbox');
+       var selectContext = Polymer.dom(document).querySelector('#px_context_browser_1');
+       var searchTextboxfilter = document.querySelector('#columnBrowser> div.columns-3.column-index-2>div:nth-child(3).px-context-browser px-context-browser-typeahead>div input.px-context-browser-typeahead-searchbox');
        searchTextboxfilter.addEventListener('input', function(e){
-         var myUL = Polymer.dom(columnsContainer).querySelector('div.columns-3.column-index-2> div:nth-child(3) ul');
+         var myUL = document.querySelector('#columnBrowser> div.columns-3.column-index-2> div:nth-child(3) ul');
          flush(function() {
-           var listItems = Polymer.dom(myUL).querySelectorAll('li');
+           listItems = Polymer.dom(myUL).querySelectorAll('li');
            assert.equal(myUL.getElementsByTagName('li').length, listItems.length);
            var list = myUL.getElementsByTagName('li').length;
            assert.isTrue(list === 1);
@@ -54,10 +52,10 @@ function runCustomTests() {
      });
 
      test('Check that clicking on the close icon clears the search box ', function(done) {
-      var columnsContainer = Polymer.dom(selectContext.root).querySelector('#columnBrowser');
-      var pxContextTypeaheadComp = Polymer.dom(columnsContainer).querySelector('.px-context-browser-typeahead-0');
-      var searchTextbox = Polymer.dom(pxContextTypeaheadComp.root).querySelector('.px-context-browser-typeahead-searchbox');
-      var closeIcon = Polymer.dom(pxContextTypeaheadComp.root).querySelector('.px-context-browser-typeahead-close-icon');
+       var selectContext = Polymer.dom(document).querySelector('#px_context_browser_1');
+      var searchTextbox = selectContext.querySelector('.px-context-browser-typeahead-searchbox');
+      var closeIcon = selectContext.querySelector('.px-context-browser-typeahead-close-icon');
+       var textVal = searchTextbox.getAttribute('placeholder');
       closeIcon.addEventListener('click', function() {
         flush(function() {
           assert.isTrue(searchTextbox.value === "");
@@ -68,14 +66,13 @@ function runCustomTests() {
     });
 
      test('Check that when show-column-typeahead is false it should not show the search box', function(done) {
+       var selectContext = Polymer.dom(document).querySelector('#px_context_browser_1');
        var selectContext2 = Polymer.dom(document).querySelector('#px_context_browser_3');
-       var clickableHeader = Polymer.dom(selectContext2.root).querySelector('h1');
+       var clickableHeader = selectContext2.querySelector('h1');
        clickableHeader.addEventListener('click', function expandListener() {
          flush(function() {
-           var columnsContainer = Polymer.dom(selectContext2.root).querySelector('#columnBrowser');
-           var pxContextTypeaheadComp = Polymer.dom(columnsContainer).querySelector('.px-context-browser-typeahead-0');
-           //var searchTextbox = Polymer.dom(pxContextTypeaheadComp.root).querySelector('.px-context-browser-typeahead-searchbox');
-           assert.isTrue(pxContextTypeaheadComp === null);
+           var searchTextbox = selectContext2.querySelector('.px-context-browser-typeahead-searchbox');
+           assert.isTrue(searchTextbox === null);
            done();
          });
        });
