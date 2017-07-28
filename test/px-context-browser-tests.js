0a3f2a8c715e5533cf91ee749013b3520c629005
Tali Marcus <tali.marcus@ge.com>
2017-07-28 14:00:56 -0700

    fix bug where data-binding 'opened' into context browser breaks active reset
    This bug was found while building the demo page for px-context-browser. When a selectedRoute was set through data-binding and opened was set to false through data-binding, the context browser would correctly open to that item, but then when the user navigated away from that item, closed the context browser and quickly reopened it in less than 5 sec, they would be brought back to the selected item rather than the view they were just looking at.

--- test/px-context-browser-tests.js
+++ test/px-context-browser-tests.js
@@ -20,5 +20,137 @@ function runCustomTests() {
         done();
       }, 250);
     });
+
+    it('resets its active item to the parent of the selected item 5s after it is closed', function(done) {
+      var fx = fixture('ContextBrowserBindingFixture');
+      var boundEl = fx.querySelector('#app');
+      var triggerEl = fx.querySelector('px-context-browser-trigger');
+      var browserEl = fx.querySelector('px-context-browser');
+
+      boundEl.items = [
+        { id: "home", label: "Home" },
+        { id: "asset", label: "Assets", children: [
+          { id: "a1", label: "Asset 1" },
+          { id: "a2", label: "Asset 2" }
+        ] }
+      ];
+
+      triggerEl.click();
+
+      setTimeout(function() {
+        browserEl.selectedRoute = ['asset', 'a1'];
+      }, 250);
+      setTimeout(function() {
+        browserEl.active = null;
+        document.body.click();
+      }, 500);
+      setTimeout(function() {
+        triggerEl.click();
+      }, 6500);
+      setTimeout(function() {
+        expect(browserEl.active).to.equal(boundEl.items[1]);
+        done();
+      }, (6500 + 250));
+    });
+
+    it('resets its active item to the parent of the selected item 5s after it is closed when `opened` is data-bound in', function(done) {
+      var fx = fixture('ContextBrowserBindingFixture');
+      var boundEl = fx.querySelector('#app');
+      var triggerEl = fx.querySelector('px-context-browser-trigger');
+      var browserEl = fx.querySelector('px-context-browser');
+
+      boundEl.items = [
+        { id: "home", label: "Home" },
+        { id: "asset", label: "Assets", children: [
+          { id: "a1", label: "Asset 1" },
+          { id: "a2", label: "Asset 2" }
+        ] }
+      ];
+
+      boundEl.opened = false;
+      triggerEl.click();
+
+      setTimeout(function() {
+        browserEl.selectedRoute = ['asset', 'a1'];
+      }, 250);
+      setTimeout(function() {
+        browserEl.active = null;
+        document.body.click();
+      }, 500);
+      setTimeout(function() {
+        triggerEl.click();
+      }, 6500);
+      setTimeout(function() {
+        expect(browserEl.active).to.equal(boundEl.items[1]);
+        done();
+      }, (6500 + 250));
+    });
+
+
+    it('does not reset its active item if it is reopened after less than 5s', function(done) {
+      var fx = fixture('ContextBrowserBindingFixture');
+      var boundEl = fx.querySelector('#app');
+      var triggerEl = fx.querySelector('px-context-browser-trigger');
+      var browserEl = fx.querySelector('px-context-browser');
+
+      boundEl.items = [
+        { id: "home", label: "Home" },
+        { id: "asset", label: "Assets", children: [
+          { id: "a1", label: "Asset 1" },
+          { id: "a2", label: "Asset 2" }
+        ] }
+      ];
+
+      triggerEl.click();
+
+      setTimeout(function() {
+        browserEl.selectedRoute = ['asset', 'a1'];
+      }, 250);
+      setTimeout(function() {
+        browserEl.active = null;
+        document.body.click();
+      }, 500);
+      setTimeout(function() {
+        triggerEl.click();
+      }, 2000);
+      setTimeout(function() {
+        expect(browserEl.active).to.equal(null);
+        done();
+      }, (2000 + 250));
+    });
+
+    it('does not reset its active item if it is reopened after less than 5s when `opened` is data-bound in', function(done) {
+      var fx = fixture('ContextBrowserBindingFixture');
+      var boundEl = fx.querySelector('#app');
+      var triggerEl = fx.querySelector('px-context-browser-trigger');
+      var browserEl = fx.querySelector('px-context-browser');
+
+      boundEl.items = [
+        { id: "home", label: "Home" },
+        { id: "asset", label: "Assets", children: [
+          { id: "a1", label: "Asset 1" },
+          { id: "a2", label: "Asset 2" }
+        ] }
+      ];
+
+      boundEl.opened = false;
+      triggerEl.click();
+
+      setTimeout(function() {
+        browserEl.selectedRoute = ['asset', 'a1'];
+      }, 250);
+      setTimeout(function() {
+        browserEl.active = null;
+        document.body.click();
+      }, 500);
+      setTimeout(function() {
+        triggerEl.click();
+      }, 2000);
+      setTimeout(function() {
+        expect(browserEl.active).to.equal(null);
+        done();
+      }, (2000 + 250));
+    });
+
   });
 }
